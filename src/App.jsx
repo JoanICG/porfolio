e3fea96fda7bfb5146d2ba117718b969bc7f62c4
JoanICG <96436880+JoanICG@users.noreply.github.com>
2025-07-18 20:36:45 +0200

    update styles and links for personal portfolio; change logo hover effects and update content

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,22 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Personal porfolio in </h1>
+      <progress></progress>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => setCount("kaboom")}> 
+          {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Work in progress
       </p>
     </>
   )
